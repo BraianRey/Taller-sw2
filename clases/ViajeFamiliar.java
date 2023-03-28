417aa6775b48646d760772939cb38bef16e80336
BraianRey <108907219+BraianRey@users.noreply.github.com>
2023-03-27 23:53:40 -0500

    Update ViajeFamiliar.java

--- clases/ViajeFamiliar.java
+++ clases/ViajeFamiliar.java
@@ -1,19 +1,23 @@
+package com.mycompany.taller_poli;;
+
+import java.util.Date;
+
 public class ViajeFamiliar extends Viaje {
     private int familia;
 
-    public ViajeFamiliar(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada, int familia) {
+    ViajeFamiliar(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada, int familia) {
         super(origen, destino, costo, fechaSalida, fechaLlegada);
         this.familia = familia;
     }
-
+    
     @Override
     public String descripcion() {
-        return "Viaje para disfrutar con toda tu familia";
+        return "Viaje para disfrutar con toda tu familia"; 
     }
-
+    
     @Override
     public String cualquierMetodo2() {
-        return "Método implementado en la clase hija viaje familiar";
+        return "Metodo implementado en la clase hija viaje familiar";
     }
 
     public int getFamilia() {
@@ -23,5 +27,4 @@ public class ViajeFamiliar extends Viaje {
     public void setFamilia(int familia) {
         this.familia = familia;
     }
-
 }
